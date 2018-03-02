29b6fad29b4d7a16613e1592ee6288a22985743c
guanxuyi <guanxuyi@bu.edu>
2018-03-02 15:39:22 -0500

    1

--- assignment4.R
+++ assignment4.R
@@ -29,122 +29,32 @@ who
 #> #   newrel_f3544 <int>, newrel_f4554 <int>, newrel_f5564 <int>,
 #> #   newrel_f65 <int>
 
+##1
 who1 <- who %>% 
   gather(new_sp_m014:newrel_f65, key = "key", value = "cases", na.rm = TRUE)
-who1
-#> # A tibble: 76,046 × 6
-#>       country  iso2  iso3  year         key cases
-#> *       <chr> <chr> <chr> <int>       <chr> <int>
-#> 1 Afghanistan    AF   AFG  1997 new_sp_m014     0
-#> 2 Afghanistan    AF   AFG  1998 new_sp_m014    30
-#> 3 Afghanistan    AF   AFG  1999 new_sp_m014     8
-#> 4 Afghanistan    AF   AFG  2000 new_sp_m014    52
-#> 5 Afghanistan    AF   AFG  2001 new_sp_m014   129
-#> 6 Afghanistan    AF   AFG  2002 new_sp_m014    90
-#> # ... with 7.604e+04 more rows
-
-who1 %>% 
-  count(key)
-#> # A tibble: 56 × 2
-#>            key     n
-#>          <chr> <int>
-#> 1  new_ep_f014  1032
-#> 2 new_ep_f1524  1021
-#> 3 new_ep_f2534  1021
-#> 4 new_ep_f3544  1021
-#> 5 new_ep_f4554  1017
-#> 6 new_ep_f5564  1017
-#> # ... with 50 more rows
 
 who2 <- who1 %>% 
   mutate(key = stringr::str_replace(key, "newrel", "new_rel"))
-who2
-#> # A tibble: 76,046 × 6
-#>       country  iso2  iso3  year         key cases
-#>         <chr> <chr> <chr> <int>       <chr> <int>
-#> 1 Afghanistan    AF   AFG  1997 new_sp_m014     0
-#> 2 Afghanistan    AF   AFG  1998 new_sp_m014    30
-#> 3 Afghanistan    AF   AFG  1999 new_sp_m014     8
-#> 4 Afghanistan    AF   AFG  2000 new_sp_m014    52
-#> 5 Afghanistan    AF   AFG  2001 new_sp_m014   129
-#> 6 Afghanistan    AF   AFG  2002 new_sp_m014    90
-#> # ... with 7.604e+04 more rows
+
 
 who3 <- who2 %>% 
   separate(key, c("new", "type", "sexage"), sep = "_")
-who3
-#> # A tibble: 76,046 × 8
-#>       country  iso2  iso3  year   new  type sexage cases
-#> *       <chr> <chr> <chr> <int> <chr> <chr>  <chr> <int>
-#> 1 Afghanistan    AF   AFG  1997   new    sp   m014     0
-#> 2 Afghanistan    AF   AFG  1998   new    sp   m014    30
-#> 3 Afghanistan    AF   AFG  1999   new    sp   m014     8
-#> 4 Afghanistan    AF   AFG  2000   new    sp   m014    52
-#> 5 Afghanistan    AF   AFG  2001   new    sp   m014   129
-#> 6 Afghanistan    AF   AFG  2002   new    sp   m014    90
-#> # ... with 7.604e+04 more rows
+
 
 who3 %>% 
   count(new)
-#> # A tibble: 1 × 2
-#>     new     n
-#>   <chr> <int>
-#> 1   new 76046
+
 who4 <- who3 %>% 
   select(-new, -iso2, -iso3)
 
 who5 <- who4 %>% 
   separate(sexage, c("sex", "age"), sep = 1)
-who5
-#> # A tibble: 76,046 × 6
-#>       country  year  type   sex   age cases
-#> *       <chr> <int> <chr> <chr> <chr> <int>
-#> 1 Afghanistan  1997    sp     m   014     0
-#> 2 Afghanistan  1998    sp     m   014    30
-#> 3 Afghanistan  1999    sp     m   014     8
-#> 4 Afghanistan  2000    sp     m   014    52
-#> 5 Afghanistan  2001    sp     m   014   129
-#> 6 Afghanistan  2002    sp     m   014    90
-#> # ... with 7.604e+04 more rows
 
-who %>%
-  gather(code, value, new_sp_m014:newrel_f65, na.rm = TRUE) %>% 
-  mutate(code = stringr::str_replace(code, "newrel", "new_rel")) %>%
-  separate(code, c("new", "var", "sexage")) %>% 
-  select(-new, -iso2, -iso3) %>% 
-  separate(sexage, c("sex", "age"), sep = 1)
-##exercise1
-##In this case study I set na.rm = TRUE just to make it easier 
-##to check that we had the correct values. Is this reasonable? 
-##Think about how missing values are represented in this dataset. 
-##Are there implicit missing values? What’s the difference 
-##between an NA and zero?
-who1 %>%
-  filter(cases == 0) %>%
-  nrow()
-gather(who, new_sp_m014:newrel_f65, key = "key", value = "cases") %>%
-  group_by(country, year)  %>%
-  mutate(missing = is.na(cases)) %>%
-  select(country, year, missing) %>%
-  distinct() %>%
-  group_by(country, year) %>%
-  filter(n() > 1)
-##exercise2
-##What happens if you neglect the mutate() step? 
-##(mutate(key = stringr::str_replace(key, "newrel", "new_rel")))
-who3a <- who1 %>%
-  separate(key, c("new", "type", "sexage"), sep = "_")
-filter(who3a, new == "newrel") %>% head()
-##exercise3
-##I claimed that iso2 and iso3 were redundant with country. 
-##Confirm this claim.
 select(who3, country, iso2, iso3) %>%
   distinct() %>%
   group_by(country) %>%
   filter(n() > 1)
-##exercise4
-##For each country, year, and sex compute the total number 
-##of cases of TB. Make an informative visualization of the data.
+
 who5 %>%
   group_by(country, year, sex) %>%
   filter(year > 1995) %>%
@@ -152,3 +62,77 @@ who5 %>%
   unite(country_sex, country, sex, remove = FALSE) %>%
   ggplot(aes(x = year, y = cases, group = country_sex, colour = sex)) +
   geom_line()
+
+##2
+?tibble::enframe
+enframe(c(a = 5, b = 7))
+
+##3(table4 to table6)
+library(foreign)
+library(stringr)
+library(plyr)
+library(reshape2)
+
+# Load data -----------------------------------------------------------------
+
+pew <- read.spss("pew.sav")
+pew <- as.data.frame(pew)
+
+
+religion <- pew[c("q16", "reltrad", "income")]
+religion$reltrad <- as.character(religion$reltrad)
+religion$reltrad <- str_replace(religion$reltrad, " Churches", "")
+religion$reltrad <- str_replace(religion$reltrad, " Protestant", " Prot")
+religion$reltrad[religion$q16 == " Atheist (do not believe in God) "] <- "Atheist"
+religion$reltrad[religion$q16 == " Agnostic (not sure if there is a God) "] <- "Agnostic"
+religion$reltrad <- str_trim(religion$reltrad)
+religion$reltrad <- str_replace_all(religion$reltrad, " \\(.*?\\)", "")
+
+religion$income <- c("Less than $10,000" = "<$10k", 
+                     "10 to under $20,000" = "$10-20k", 
+                     "20 to under $30,000" = "$20-30k", 
+                     "30 to under $40,000" = "$30-40k", 
+                     "40 to under $50,000" = "$40-50k", 
+                     "50 to under $75,000" = "$50-75k",
+                     "75 to under $100,000" = "$75-100k", 
+                     "100 to under $150,000" = "$100-150k", 
+                     "$150,000 or more" = ">150k", 
+                     "Don't know/Refused (VOL)" = "Don't know/refused")[religion$income]
+
+religion$income <- factor(religion$income, levels = c("<$10k", "$10-20k", "$20-30k", "$30-40k", "$40-50k", "$50-75k", 
+                                                      "$75-100k", "$100-150k", ">150k", "Don't know/refused"))
+
+counts <- count(religion, c("reltrad", "income"))
+names(counts)[1] <- "religion"
+library(tidyr)
+library(dplyr)
+table4<-dcast(counts,religion~income)
+table6<-gather(table4,key="income",value="freq",2:11)
+table6<-arrange(table6,religion)
+##4
+options(stringsAsFactors = FALSE)
+
+table7 <- read.csv("billboard.csv")
+table7 <- table7[, c("year", "artist.inverted", "track", "time", "date.entered", "x1st.week", "x2nd.week", "x3rd.week", "x4th.week", "x5th.week", "x6th.week", "x7th.week", "x8th.week", "x9th.week", "x10th.week", "x11th.week", "x12th.week", "x13th.week", "x14th.week", "x15th.week", "x16th.week", "x17th.week", "x18th.week", "x19th.week", "x20th.week", "x21st.week", "x22nd.week", "x23rd.week", "x24th.week", "x25th.week", "x26th.week", "x27th.week", "x28th.week", "x29th.week", "x30th.week", "x31st.week", "x32nd.week", "x33rd.week", "x34th.week", "x35th.week", "x36th.week", "x37th.week", "x38th.week", "x39th.week", "x40th.week", "x41st.week", "x42nd.week", "x43rd.week", "x44th.week", "x45th.week", "x46th.week", "x47th.week", "x48th.week", "x49th.week", "x50th.week", "x51st.week", "x52nd.week", "x53rd.week", "x54th.week", "x55th.week", "x56th.week", "x57th.week", "x58th.week", "x59th.week", "x60th.week", "x61st.week", "x62nd.week", "x63rd.week", "x64th.week", "x65th.week", "x66th.week", "x67th.week", "x68th.week", "x69th.week", "x70th.week", "x71st.week", "x72nd.week", "x73rd.week", "x74th.week", "x75th.week", "x76th.week")]
+names(table7)[2] <- "artist"
+
+table7$artist <- iconv(table7)
+table7$track <- str_replace(table7$track, " \\(.*?\\)", "")
+names(table7)[-(1:5)] <- str_c("wk", 1:76)
+table7 <- arrange(table7, year, artist, track)
+
+long_name <- nchar(table7$track) > 20
+table7$track[long_name] <- paste0(substr(table7$track[long_name], 0, 20), "...")
+
+names(table7)[6:81] <- (1:76)
+table8 <- gather(table7,key = "week",value = "rank",6:81,na.rm = T)
+table8$date.entered <- julian(as.Date(table8$date.entered))
+table8$date.entered <- table8$date.entered + (as.numeric(table8$week)-1)*7
+table8$date.entered <- as.Date(table8$date.entered, origin = "1970-01-01")                               
+names(table8)[5] <- "date"
+table8 <- arrange(table8,track)
+table8 <- arrange(table8,artist)
+
+
+
+
